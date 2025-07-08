d3309b530d7b987049e04ac7fb9df1b5009998f2
Peter Seibel <peter@gigamonkeys.com>
2025-07-08 11:18:17 -0700

    Reviving Turtle code from deleted _sources dir.

---
+++ TurtleJavaHTMLCode/combine.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cat DigitalPicture.java
+cat ModelDisplay.java
+cat PathSegment.java
+cat Pen.java
+cat Picture.java
+cat Pixel.java
+cat SimplePicture.java
+cat SimpleTurtle.java
+cat Turtle.java
+cat Giffer.java
+cat World.java
+#cat TurtleTest.java
