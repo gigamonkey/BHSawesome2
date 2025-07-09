6750dafe471a54a47d29f451d7508afb4f03ed3f
Peter Seibel <peter@gigamonkeys.com>
2025-07-09 06:52:46 -0700

    Let cleanup work on multiple files.

--- cleanup.sh
+++ cleanup.sh
@@ -2,12 +2,12 @@
 
 set -euo pipefail
 
-file="$1"
-
-out=$(mktemp)
-if xsltproc cleanup.xsl "$file" > "$out"; then
-    mv "$out" "$file"
-    uv run format-ptx.py -i "$file"
-else
-    echo "Problem processing $file"
-fi
+for file in "$@"; do
+    out=$(mktemp)
+    if xsltproc cleanup.xsl "$file" > "$out"; then
+        mv "$out" "$file"
+        uv run format-ptx.py -i "$file"
+    else
+        echo "Problem processing $file"
+    fi
+done
