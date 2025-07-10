34693073e731cf55fd13759d2df6db6e7410151d
Peter Seibel <peter@gigamonkeys.com>
2025-07-10 09:08:36 -0700

    Cleaning up words script.

--- words.py
+++ words.py
@@ -2,20 +2,29 @@
 
 import re
 from argparse import ArgumentParser
-from sys import stderr, stdout
-from textwrap import fill, dedent, indent
-from xml.sax.saxutils import escape, quoteattr
 
 from lxml import etree
 
 
-def count_chapter(c):
-    print(f"- {title(c)} ({count_paragraphs(c):,})\n")
+def count_level(c, subs, level, parent_count):
+    count = count_paragraphs(c)
 
-    count_intro(c.xpath("introduction")[0])
+    emit(title(c), count, parent_count, level)
 
-    for s in c.xpath(".//section"):
-        count_section(s)
+    intros = c.xpath("./introduction")
+
+    if len(intros) > 0:
+        emit("Introduction", count_paragraphs(intros[0]), count, level + 1)
+
+    if subs:
+        for s in c.xpath(f"./{subs[0]}"):
+            count_level(s, subs[1:], level + 1, count)
+
+
+def emit(title, count, parent_count, level):
+    indent = " " * (level * 2)
+    percent = 100 * count / parent_count
+    print(f"{indent}- {title} ({count:,} - {percent:.2f}%)\n")
 
 
 def title(elem):
@@ -24,14 +33,6 @@ def title(elem):
 
 def count_paragraphs(e):
     return sum(wordcount(p) for p in e.xpath(".//p"))
-
-
-def count_intro(i):
-    print(f"  - Introduction ({count_paragraphs(i):,})\n")
-
-
-def count_section(s):
-    print(f"  - {title(s)} ({count_paragraphs(s):,})\n")
 
 
 def wordcount(e):
@@ -46,10 +47,16 @@ def main(filename):
     tree = etree.parse(filename)
     tree.xinclude()
 
+    total = count_paragraphs(tree.getroot())
+
     chapters = tree.xpath("//chapter")
 
+    print("-*- mode: list-outline; -*-\n")
+
+    print(f"Total words: {total:,}\n")
+
     for c in chapters:
-        count_chapter(c)
+        count_level(c, ["section", "subsection"], 0, total)
 
 
 if __name__ == "__main__":
