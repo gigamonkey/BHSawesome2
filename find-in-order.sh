db6539436e4094ee68549fc0f78421bda793ea0c
Peter Seibel <peter@gigamonkeys.com>
2025-08-09 10:00:57 -0700

    Use case insensitive search in find-in-order

--- find-in-order.sh
+++ find-in-order.sh
@@ -7,7 +7,7 @@ pat="$1"
 # Ripgrep normally processes things in parallel so output can come out in any order.
 # So we loop through the files and rg them one at a time.
 ./list-files.py --full pretext/full-main.ptx | while read -r f; do
-    if rg --pretty --with-filename "$1" "$f"; then
+    if rg --ignore-case --pretty --with-filename "$1" "$f"; then
         echo ""
     fi
     #rg -l "$1" "$f" || true
